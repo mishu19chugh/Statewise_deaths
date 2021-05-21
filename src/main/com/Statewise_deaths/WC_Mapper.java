41d03f6e07a0044df0e9fb3a12a4fd991f12ef6c
Mishu Chugh <mishu.chugh@amdocs.com>
2021-05-21 16:43:01 +0530

    here changed the code

--- src/main/com/Statewise_deaths/WC_Mapper.java
+++ src/main/com/Statewise_deaths/WC_Mapper.java
@@ -1,6 +1,8 @@
 package com.Statewise_deaths;
 
 import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -8,6 +10,7 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.mortbay.util.StringUtil;
 
 public class WC_Mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>
 {
@@ -24,7 +27,8 @@ public class WC_Mapper extends MapReduceBase implements Mapper<LongWritable,Text
         }
 
         String[] DeathsData=valueString.split(",");
-        int death= Integer.parseInt(DeathsData[2]);
+        /*int death= Integer.parseInt(DeathsData[2]);*/
+        int death=StringUtils.isNotBlank(DeathsData[2])?Integer.parseInt(DeathsData[2]):0;
         output.collect(new Text(DeathsData[1]),new IntWritable(death));
     }
 }
